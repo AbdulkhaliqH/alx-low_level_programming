1c29cc3b5a752c03e8cfd323c787ecad271577b9
HabissKhal <khaleeqhabiss@gmail.com>
2022-09-11 01:43:01 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 /**
 * main - print the string in the put function
 *
-* Description: using the main funtion
-* this program prints "programming is like building a multilingual puzzle
-* Return: 0
+* Description: using the main function
+* this program prints "programming is like a multilingual puzzle
+* Return 0;
 */
 int main(void)
 {
