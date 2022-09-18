a514211e53543aa6d0fa0401cd5dd376f9865e39
HabissKhal <khaleeqhabiss@gmail.com>
2022-09-18 09:02:20 -0700

    prints the alphabet in lowercase

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,10 +7,10 @@ int main(void)
 {
 char abd;
 for (abc = 'a'; abc <= 'z'; abc++)
-{
-if ((abc != 'e') && (abc != 'q'))
-putchar(abc);
-}
+	{
+	if ((abc != 'e') && (abc != 'q'))
+	putchar(abc);
+	}
 putchar('\n');
 return (0);
 }
