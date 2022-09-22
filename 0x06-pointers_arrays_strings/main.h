9db6d282d554968c8eaecf5279037af7967f0830
HabissKhal <khaleeqhabiss@gmail.com>
2022-09-22 07:49:30 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,23 +1,24 @@
-	1.	char *_strncat(char *dest, char *src, int n);
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+/**
+ * void prototypes()
+ * int prototypes()
+ * char prototypes()
+ */
+int _putchar(char c);
+void reverse_array(int *a, int n);
+void print_number(int n);
+void print_buffer(char *b, int size);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
-	2.	char *_strcat(char *dest, char *src);
-
-	3.	char *_strncpy(char *dest, char *src, int n);
-
-	4.	int _strcmp(char *s1, char *s2);
-
-	5.	void reverse_array(int *a, int n);
-
-	6.	char *string_toupper(char *);
-
-	7.	char *cap_string(char *);
-
-	8.	char *rot13(char *);
-
-	9.	void print_number(int n);
-
-	10.	char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
-	11.	void print_buffer(char *b, int size);
-
-	12.	int _putchar(char c);
+#endif /*MAIN_H*/
