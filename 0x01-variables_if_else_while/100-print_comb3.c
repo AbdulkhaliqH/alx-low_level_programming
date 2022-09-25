2c3968e42467f4fbfe26ea9241fa3d5f5e277368
HabissKhal <khaleeqhabiss@gmail.com>
2022-09-25 00:41:31 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,12 +8,12 @@ int main(void)
 int numR;
 int numL;
 int minL = 48;
-for (numL = 48; numL <= 57; numL++)
-{
 for (numR = minL++; numR <= 57; numR++)
 {
+for (numL = 48; numL <= 57; numL++)
+{
 if (numR != numL)
-}
+{
 putchar(numL);
 putchar(numR);
 if (!(numR == 57 && numL == 56))
